d38b97003d1201c5207506b928c96791c51f2e8e
TheFakeAccount <74877345+TheFakeBot@users.noreply.github.com>
2026-07-18 21:07:25 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
+function input(text)
+    print(text)
+    read()
+end
+
+function update(link,filename)
+    f = fs.open(filename, "w+")
+    dat = http.get(link)
+    sleep(1)
+    f.write(dat)
+    f.close()
+end
+
 while true do
     i = input(">")
     shell.run("i")
